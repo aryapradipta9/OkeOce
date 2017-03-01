4c85401eb232eec6bf35425678751f66ab267fc9
aryapradipta9 <aryapradipta9@gmail.com>
2017-03-01 22:04:41 +0700

    add animal.h

---
+++ animal.h
@@ -0,0 +1,17 @@
+// Author : 13515017 / Putu Arya Pradipta
+// File : animal.h
+
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+class Animal{
+
+  public:
+    Animal();
+    ShowExperience();
+  protected:
+    const char* Exp;
+    const char* Family;
+    const char* Species;
+}
+#endif
