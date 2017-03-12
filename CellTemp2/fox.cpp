ee2e98a55379369775982583409edc2b7a27c29b
fathur2707 <mfathur2707@gmail.com>
2017-03-12 21:22:02 +0900

    Update fox.cpp

--- CellTemp2/fox.cpp
+++ CellTemp2/fox.cpp
@@ -5,7 +5,7 @@
 Fox::Fox()
 {
 	Animal::Species = "Vulpes lagopus";
-	Animal::Experience = "What does the fox says";
+	Animal::Experience = "What does the fox says"; //MIKOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON !
 	Animal::Berat = 60;
 	Animal::AniChar = 'B';
 }
